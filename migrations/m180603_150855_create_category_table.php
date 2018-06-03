4fc81ccd33933bd7b9e6ff08d1d6ad85de8fd641
Intruder101010 <muzikant18@gmail.com>
2018-06-03 18:26:20 +0300

    Была создана база, таблицы и миграции

---
+++ migrations/m180603_150855_create_category_table.php
@@ -0,0 +1,25 @@
+<?php
+use yii\db\Migration;
+/**
+ * Handles the creation of table `category`.
+ */
+class m180603_150855_create_category_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('category', [
+            'id' => $this->primaryKey(),
+            'title'=>$this->string()
+        ]);
+    }
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('category');
+    }
+}
